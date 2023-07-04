d4f397e6621cfc0767c133d6c1c8fa9a765a20ad
levantosina <levantosina1992@gmail.com>
2023-07-04 15:33:56 +0300

    There were added spring-boot-starter-webflux dependency to pom.xml and CustomerIntegrationTest.

---
+++ src/test/java/com/app/journey/CustomerIntegrationTest.java
@@ -0,0 +1,14 @@
+package com.app.journey;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+/**
+ * @author Levantosina
+ */
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class CustomerIntegrationTest {
+
+
+}

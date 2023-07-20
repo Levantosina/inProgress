27eaff3a287fbbc9f8233452aa235de8b04b9689
levantosina <levantosina1992@gmail.com>
2023-07-20 19:37:27 +0300

    CD with testControllerv10

---
+++ backend/src/main/java/com/app/testController.java
@@ -0,0 +1,17 @@
+package com.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Levantosina
+ */
+@RestController
+public class testController {
+    record test(String name){}
+
+    @GetMapping("/test")
+    public test testController() {
+        return  new test("testController");
+    }
+}

c9f933b4ab94c0ad46ff308ee8d99a5b96257c20
levantosina <levantosina1992@gmail.com>
2023-07-23 18:10:02 +0300

    com1

--- backend/src/main/java/com/app/testController.java
+++ backend/src/main/java/com/app/testController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class testController {
     record test(String name){}
 
-    @GetMapping("/testTest2")
+    @GetMapping("/testTest3")
     public test testController() {
         return  new test("testController");
     }

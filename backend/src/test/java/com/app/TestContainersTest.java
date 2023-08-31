833f7163159fdc74cb35b1ae65cb2511c06eaa6f
levantosina <levantosina1992@gmail.com>
2023-08-31 16:39:39 +0300

    test

--- backend/src/test/java/com/app/TestContainersTest.java
+++ backend/src/test/java/com/app/TestContainersTest.java
@@ -12,8 +12,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class TestContainersTest extends AbstractTestContainers {
     @Test
     void canStartPostgresDB() {
-
-
         assertThat(postgreSQLContainer.isRunning()).isTrue();
         assertThat(postgreSQLContainer.isRunning()).isTrue();
     }

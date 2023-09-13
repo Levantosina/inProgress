a0a29a49b2fed6cf87ae7b068f1f5cd315b15798
levantosina <levantosina1992@gmail.com>
2023-09-13 11:41:36 +0300

    refactor

--- backend/src/main/java/com/app/customer/CustomerRegistrationRequest.java
+++ backend/src/main/java/com/app/customer/CustomerRegistrationRequest.java
@@ -4,8 +4,11 @@ package com.app.customer;
  * @author Levantosina
  */
 public record CustomerRegistrationRequest (
-    String name,
-    String email,
-    Integer age,
-    Gender gender
-) { }
+        String name,
+        String email,
+        String password,
+        Integer age,
+        Gender gender
+) {
+
+}

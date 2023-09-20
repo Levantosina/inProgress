df3039884d37958e02a4f626c547dbfa96d53bb2
levantosina <levantosina1992@gmail.com>
2023-09-20 11:15:22 +0300

    added TestConfig

---
+++ backend/src/test/java/com/app/TestConfig.java
@@ -0,0 +1,20 @@
+package com.app;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author Levantosina
+ */
+@TestConfiguration
+public class TestConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
+
+

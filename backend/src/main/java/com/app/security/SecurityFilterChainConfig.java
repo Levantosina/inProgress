a7e84a50e90a171087260b1ebc7bf20dcda7b29c
levantosina <levantosina1992@gmail.com>
2023-09-26 14:56:29 +0300

    allow /test to have no auth

--- backend/src/main/java/com/app/security/SecurityFilterChainConfig.java
+++ backend/src/main/java/com/app/security/SecurityFilterChainConfig.java
@@ -37,6 +37,8 @@ public class SecurityFilterChainConfig {
                 .authorizeHttpRequests()
                 .requestMatchers(HttpMethod.POST,"/api/v1/customers","/api/v1/auth/login")
                 .permitAll()
+                .RequestMatchers(HttpMethod.GET,"/test")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()

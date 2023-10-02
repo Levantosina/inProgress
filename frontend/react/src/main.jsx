f2e42b9bfbd41da1d9ce606d8419503a7fb12c4e
levantosina <levantosina1992@gmail.com>
2023-10-02 18:37:36 +0300

    auth template settings

--- frontend/react/src/main.jsx
+++ frontend/react/src/main.jsx
@@ -3,16 +3,29 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { ChakraProvider } from '@chakra-ui/react'
 import { createStandaloneToast } from '@chakra-ui/react'
+import { createBrowserRouter,RouterProvider} from "react-router-dom";
+import Login from "./components/login/Login.jsx";
 import './index.css'
 
 
-const { ToastContainer, toast } = createStandaloneToast()
+const { ToastContainer } = createStandaloneToast()
+
+const router = createBrowserRouter([
+    {
+       path: "/", //main page of the app
+         element:<Login/>
+    },
+    {
+        path:"/dashboard",
+        element:<App />
+    }
+])
 ReactDOM
     .createRoot(document.getElementById('root'))
     .render(
   <React.StrictMode>
       <ChakraProvider>
-          <App />
+         <RouterProvider router={router}/>
           <ToastContainer />
         </ChakraProvider>
   </React.StrictMode>,

ee6f5af954a91be51e7c72d52a8b112442a06db1
levantosina <levantosina1992@gmail.com>
2023-10-04 17:01:48 +0300

    implemented AuthProvider and ProtectedRoute

--- frontend/react/src/main.jsx
+++ frontend/react/src/main.jsx
@@ -5,7 +5,10 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { createStandaloneToast } from '@chakra-ui/react'
 import { createBrowserRouter,RouterProvider} from "react-router-dom";
 import Login from "./components/login/Login.jsx";
+import AuthProvider from "./components/context/AuthContext.jsx";
 import './index.css'
+import ProtectedRoute from "./components/shared/protectedRoute.jsx";
+
 
 
 const { ToastContainer } = createStandaloneToast()
@@ -17,16 +20,18 @@ const router = createBrowserRouter([
     },
     {
         path:"/dashboard",
-        element:<App />
+        element:<ProtectedRoute><App /></ProtectedRoute>
     }
 ])
 ReactDOM
     .createRoot(document.getElementById('root'))
     .render(
-  <React.StrictMode>
-      <ChakraProvider>
-         <RouterProvider router={router}/>
-          <ToastContainer />
-        </ChakraProvider>
-  </React.StrictMode>,
+        <React.StrictMode>
+            <ChakraProvider>
+                <AuthProvider>
+                    <RouterProvider router={router} />
+                </AuthProvider>
+                <ToastContainer />
+            </ChakraProvider>
+        </React.StrictMode>,
 )
